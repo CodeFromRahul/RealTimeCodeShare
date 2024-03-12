e49fa9f76d356bc4f3a3844bc34c88b1ba0728a2
CodeFromRahul <rahulvish194002@gmail.com>
2024-03-13 02:17:08 +0530

    Real time Code sharing platform using React

---
+++ src/pages/Editorpage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Editorpage = () => {
+  return (
+    <div>Editor page</div>
+  )
+}
+
+export default Editorpage
---
+++ src/pages/Home.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import {v4 as uuidv4} from "uuid";
+import  toast from 'react-hot-toast'
+import {  useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    const Navigate =useNavigate();
+const [roomid,setRoomId]=useState('');
+
+
+    const createNewRoom=(e)=>{
+        e.preventDefault();
+        const id = uuidv4().substr(0, 5).toUpperCase();
+        // console.log(id);
+        setRoomId(id);
+        toast.success('Created New Room');
+
+
+    }
+
+    const joinRoom=()=>{
+        if(!roomid ){
+            toast.error("Enter Room ID")
+        }
+        else{
+            toast.success("Room Joined")
+
+        }
+        
+
+        // Redirect to room
+        Navigate(`/editor/${roomid}`,{
+            state:{
+            roomid,
+            
+            }
+        })
+        
+
+    }
+
+    const handleInputEnter=(e)=>{
+       
+       
+        if(e.code ==='Enter'){
+           joinRoom();
+        }
+    }
+  return (
+    <div className='homePageWrapper'>
+        <div className='formwrapper'>
+    <img src='/favicon.ico' alt='Pulp logo'/>
+    <h4 className='mainLabel '>paste invitation code </h4>
+        <div className='inputCode'>
+            <input type="text" className='inputBox' onChange={(e)=>setRoomId(e.target.value)} value={roomid} placeholder='ENTER ID ' onKeyUp={handleInputEnter} ></input>
+            <div className='inputBtn'>
+            <button onClick={joinRoom} className='btn joinbtn'>Join Room</button>
+            <button onClick={createNewRoom}  className='btn btn2 joinbtn  '>Create Room</button>
+            </div>
+        </div>
+        </div>
+        <footer>
+            <h4>BUILT WITH❤️❤️❤️</h4>
+        </footer>
+    </div>
+  )
+}
+
+export default Home
