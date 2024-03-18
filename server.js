59d1e05acf9b9a2ecf3c94637388f806826c7782
Rahul <rahulvish194002@gmail.com>
2024-03-19 00:54:27 +0530

    dynamic update

--- server.js
+++ server.js
@@ -2,21 +2,46 @@ const express = require('express');
 
 const app= express();
 const http = require('http');
-const {Server} = require('socket.io')
+const {Server} = require('socket.io');
+const ACTIONS = require('./src/Actions');
 
 
 
 const server = http.createServer(app);
 const io = new Server(server);
 
+const userSocketMap ={};
+function getAllConnectedClients(roomid){
+    // Mapping Used
+   return  Array.from(io.sockets.adapter.rooms.get(roomid) || []).map((socketId)=>{
+return {
+    socketId,
+    roomid:userSocketMap[socketId],
+};
+   });
+}
+
 io.on('connection',(socket)=>{
     console.log('socket connected',socket.id);
+    socket.on(ACTIONS.JOIN,({roomid})=>{
+        userSocketMap[socket.id] = roomid;
+        socket.join(roomid);
+        const clients = getAllConnectedClients(roomid);
+       console.log(clients)
+       clients.forEach(({socketId})=>{
+         io.to(socketId).emit(ACTIONS.JOIN,{
+            clients,
+            roomid,
+            socketId:socket.id,
+         })
+       })
+    });
 });
 
 
 
 
-const PORT = process.env.PORT||5000;
+const PORT = process.env.PORT||5001;
 server.listen(PORT,()=>{
     console.log(`server is listininig at ${PORT}`)
 })
--- src/pages/Editorpage.js
+++ src/pages/Editorpage.js
@@ -3,17 +3,37 @@ import Client from '../components/client';
 import Editor from '../components/Editors';
 import { initSocket } from '../socket';
 import ACTIONS from '../Actions';
-import {home} from "./Home"
+import Toast from 'react-hot-toast';
+import { useLocation , useNavigate, Navigate} from 'react-router-dom';
+
 import client from '../components/client';
+import toast from 'react-hot-toast';
 const Editorpage = () => {
   const socketRef = useRef(null);
-  
+  const location = useLocation();
+  const reactNavigator =useNavigate();
+
   useEffect(()=>{
     const init = async () => {
       socketRef.current = await initSocket();
+      socketRef.current.on('connect_error',(err)=>handleErrors(err));
+      socketRef.current.on('connect_failed',(err)=>handleErrors(err));
+
+      function handleErrors(e){
+        console.log('socket error',e);
+        Toast.error('socket connection failed ,try again later ');
+        reactNavigator('/');
+      }
+
       socketRef.current.emit(ACTIONS.JOIN,{
-        roomid,
-      })
+        roomid:location.state.roomid,
+      });
+
+      // Listining for joined event
+      socketRef.current.on(ACTIONS.JOINED,({clients,roomid,socketId})=>{})
+              if(roomid!=location.state.roomid){
+                
+              }
     };
     init();
   },[]);
@@ -25,7 +45,13 @@ const Editorpage = () => {
     {sockedid:2 },
     {sockedid:24}
   ]);
+ 
+  if(!location.state){
+    return <Navigate to="/"/>;
 
+  }
+
+  
 
 
   return (
